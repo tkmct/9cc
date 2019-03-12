7a7f127800c0f7c02681cdc8252cc84757ff9527
tkmct <1220t.takamichi@gmail.com>
2019-03-12 20:41:31 +0900

    one letter variable

--- 9cc.c
+++ 9cc.c
@@ -7,6 +7,7 @@
 enum
 {
   TK_NUM = 256,
+  TK_IDENT,
   TK_EOF,
 };
 
@@ -40,13 +41,15 @@ typedef struct
 {
   int ty;
   int val;
+  char *input;
 } Token;
 
-Token *new_token(int ty, int val)
+Token *new_token(int ty, int val, char *input)
 {
   Token *t = calloc(1, sizeof(Token));
   t->ty = ty;
   t->val = val;
+  t->input = input;
 
   return t;
 }
@@ -59,24 +62,26 @@ void tokenize(char *p)
 {
   tokens = new_vector();
 
-  int i = 0;
-  while (*p)
-  {
-    if (isspace(*p))
-    {
-      p++;
-      continue;
-    }
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')')
-    {
-      vec_push(tokens, new_token(*p, 0));
+  while (*p) {
+    if (isspace(*p)) {
       p++;
       continue;
     }
 
-    if (isdigit(*p))
-    {
-      vec_push(tokens, new_token(TK_NUM, strtol(p, &p, 10)));
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '=' || *p == ';') {
+      vec_push(tokens, new_token(*p, 0, NULL));
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      vec_push(tokens, new_token(TK_NUM, strtol(p, &p, 10), NULL));
+      continue;
+    }
+
+    if ('a' <= *p && *p <= 'z') {
+      vec_push(tokens, new_token(TK_IDENT, 0, p));
+      p++;
       continue;
     }
 
@@ -84,29 +89,39 @@ void tokenize(char *p)
     exit(1);
   }
 
-  vec_push(tokens, new_token(TK_EOF, 0));
+  vec_push(tokens, new_token(TK_EOF, 0, NULL));
 }
 
 // PARSER
-enum
-{
+enum {
   ND_NUM = 256,
+  ND_IDENT,
 };
 
-typedef struct Node
-{
+typedef struct Node {
   int ty;
   struct Node *lhs;
   struct Node *rhs;
   int val;
+  char name;
 } Node;
 
+Node *stmt();
+Node *assign();
 Node *add();
 Node *mul();
 Node *term();
 
-Node *new_node(int ty, Node *lhs, Node *rhs)
-{
+Node *code[100];
+
+void program() {
+  int i = 0;
+  while (((Token *)tokens->data[pos])->ty != TK_EOF)
+    code[i++] = stmt();
+  code[i] = NULL;
+}
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
   Node *node = malloc(sizeof(Node));
   node->ty = ty;
   node->lhs = lhs;
@@ -114,44 +129,67 @@ Node *new_node(int ty, Node *lhs, Node *rhs)
   return node;
 }
 
-Node *new_node_num(int val)
-{
+Node *new_node_num(int val) {
   Node *node = malloc(sizeof(Node));
   node->ty = ND_NUM;
   node->val = val;
   return node;
 }
 
-int consume(int ty)
-{
+Node *new_node_ident(char *ident) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_IDENT;
+  node->name = *ident;
+  return node;
+}
+
+int consume(int ty) {
   if (((Token *)tokens->data[pos])->ty != ty)
     return 0;
   pos++;
   return 1;
 }
 
-Node *term()
-{
-  if (consume('('))
-  {
+Node *stmt() {
+  Node *node = assign();
+  if (!consume(';')) {
+    fprintf(stderr, "Invalid token. expected ';' but got %s:", ((Token *)tokens->data[pos])->input);
+    exit(1);
+  }
+  return node;
+}
+
+Node *assign() {
+  Node *node = add();
+
+  if (consume('='))
+    node = new_node('=', node, assign());
+  return node;
+}
+
+Node *term() {
+  if (consume('(')) {
     Node *node = add();
-    if (!consume(')'))
+    if (!consume(')')) {
       fprintf(stderr, "No closing parentheses: %c", ((Token *)tokens->data[pos])->ty);
+      exit(1);
+    }
     return node;
   }
 
   if (((Token *)tokens->data[pos])->ty == TK_NUM)
     return new_node_num(((Token *)tokens->data[pos++])->val);
 
+  if (((Token *)tokens->data[pos])->ty == TK_IDENT)
+    return new_node_ident(((Token *)tokens->data[pos++])->input);
+
   fprintf(stderr, "Not a number nor open parentheses: %c", ((Token *)tokens->data[pos])->ty);
 }
 
-Node *mul()
-{
+Node *mul() {
   Node *node = term();
 
-  for (;;)
-  {
+  for (;;) {
     if (consume('*'))
       node = new_node('*', node, term());
     else if (consume('/'))
@@ -161,12 +199,10 @@ Node *mul()
   }
 }
 
-Node *add()
-{
+Node *add() {
   Node *node = mul();
 
-  for (;;)
-  {
+  for (;;) {
     if (consume('+'))
       node = new_node('+', node, mul());
     else if (consume('-'))
@@ -177,11 +213,41 @@ Node *add()
 }
 
 // GENERATOR
-void gen(Node *node)
-{
-  if (node->ty == ND_NUM)
-  {
+void gen_lval(Node *node) {
+  if (node->ty != ND_IDENT) {
+    perror("Invalid left value");
+    exit(1);
+  }
+
+  int offset = ('z' - node->name + 1) * 8;
+  printf("  mov rax, rbp\n");
+  printf("  sub rax, %d\n", offset);
+  printf("  push rax\n");
+}
+
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
     printf("  push %d\n", node->val);
+    return;
+  }
+
+  if (node->ty == ND_IDENT) {
+    gen_lval(node);
+    printf("  pop rax\n");
+    printf("  mov rax, [rax]\n");
+    printf("  push rax\n");
+    return;
+  }
+
+  if (node->ty == '=') {
+    gen_lval(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    printf("  mov [rax], rdi\n");
+    printf("  push rdi\n");
     return;
   }
 
@@ -191,8 +257,7 @@ void gen(Node *node)
   printf("  pop rdi\n");
   printf("  pop rax\n");
 
-  switch (node->ty)
-  {
+  switch (node->ty) {
   case '+':
     printf("  add rax, rdi\n");
     break;
@@ -210,8 +275,7 @@ void gen(Node *node)
   printf("  push rax\n");
 }
 
-int expect(int line, int expected, int actual)
-{
+int expect(int line, int expected, int actual) {
   if (expected == actual)
     return 1;
   fprintf(stderr, "%d: %d expected, but got %d\n",
@@ -219,8 +283,7 @@ int expect(int line, int expected, int actual)
   exit(1);
 }
 
-void runtest()
-{
+void runtest() {
   Vector *vec = new_vector();
   expect(__LINE__, 0, vec->len);
 
@@ -235,8 +298,7 @@ void runtest()
   printf("OK\n");
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
@@ -249,16 +311,29 @@ int main(int argc, char **argv)
 
   // Tokenize and parse
   tokenize(argv[1]);
-  Node *node = add();
+  program();
 
   // First Template of assembly
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  gen(node);
+  // prologue
+  printf("  push rbp\n");
+  printf("  mov rbp, rsp\n");
+  printf("  sub rsp, 208\n");
 
-  printf("  pop rax\n");
+  // generate code from the first line
+  for (int i = 0; code[i]; i++) {
+    gen(code[i]);
+
+    // pop remaining statement's value
+    printf("  pop rax\n");
+  }
+
+  // epilogue
+  printf("  mov rsp, rbp\n");
+  printf("  pop rbp\n");
   printf("  ret\n");
   return 0;
 }
